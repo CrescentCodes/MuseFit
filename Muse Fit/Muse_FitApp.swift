7a53707e1e52200a2c26f04146a776e6ced3daa5
MusedCodes <bleeps_college.0f@icloud.com>
2023-01-23 17:31:41 -0800

    Initial Commit

---
+++ Muse Fit/Muse_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muse_FitApp.swift
+//  Muse Fit
+//
+//  Created by Mohamed Said on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Muse_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
